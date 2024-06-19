3ed15fa6ddecb04492fe24945cbe8f86d89b2335
= <=>
2024-06-19 14:12:50 +0100

    adicionado no minímo 3 caracteres na sigla

--- src/utils/validateBanco.ts
+++ src/utils/validateBanco.ts
@@ -6,7 +6,7 @@ export const BancoSchema = z.object({
       message: "O nome precisa ter no minímo 3 caracteres",
     })
     .nonempty({ message: "O nome não pode ser enviado vázio!" }),
-  codigo: string().min(1, {
+  codigo: string().min(3, {
     message: "O nome precisa ter no mínimo 1 caractere",
   }),
   sigla: string().min(2, {

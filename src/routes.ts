d3c1d5b469cb4342d7b46435665935d76a1109c4
= <=>
2024-06-25 09:40:34 +0100

    exportado o FuncionarioRoutes no routes do server

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { BancoRoutes } from "./routes/BancoRoutes";
 import { CarreiraRoutes } from "./routes/CarreiraRoutes";
 import { CategoriaRoutes } from "./routes/CategoriaRoutes";
 import { FuncaoRoutes } from "./routes/FuncaoRoutes";
+import { FuncionarioRoutes } from "./routes/FuncionarioRoutes";
 import { SubCarreiraRoutes } from "./routes/SubCarreirasRoutes";
 
 const routes = Router();
@@ -12,4 +13,5 @@ routes.use("/banco", BancoRoutes);
 routes.use("/carreira", CarreiraRoutes);
 routes.use("/categoria", CategoriaRoutes);
 routes.use("/subcarreira", SubCarreiraRoutes);
+routes.use("/funcionario", FuncionarioRoutes);
 export { routes };
--- src/routes/FuncionarioRoutes.ts
+++ src/routes/FuncionarioRoutes.ts
@@ -1,16 +1,7 @@
 import { Router } from "express";
-import {
-  deleteCategoria,
-  getbyIdCategoria,
-  updateCategoriaController,
-} from "../controllers/Categoria/CategoriaController";
+
 import { createFuncionarioController } from "../controllers/Funcionario/funcionarioController";
-import { getAllCategoria } from "../models/Categoria/list";
 
 export const FuncionarioRoutes = Router();
 
 FuncionarioRoutes.post("/", createFuncionarioController);
-FuncionarioRoutes.get("/:id", getbyIdCategoria);
-FuncionarioRoutes.put("/:id", updateCategoriaController);
-FuncionarioRoutes.get("/", getAllCategoria);
-FuncionarioRoutes.delete("/:id", deleteCategoria);

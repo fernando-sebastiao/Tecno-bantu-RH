f51b467622253c3c69f2408a88b5109253f2d74f
= <=>
2024-06-25 10:46:29 +0100

    feito o verificar as chaves estrangeiras antes de se cadastrar um funcionario

--- src/controllers/Funcionario/funcionarioController.ts
+++ src/controllers/Funcionario/funcionarioController.ts
@@ -28,6 +28,39 @@ export const createFuncionarioController = async (
         "Este Funcionario já existe",
       ]);
     }
+    //verificar se a função existe
+    const verificarFuncao = await prisma.funcao.findFirst({
+      where: {
+        id: parseFuncionario.data.id_funcao,
+      },
+    });
+    if (!verificarFuncao) {
+      throw new CustomError("Esta Função não existe!", 400, [
+        "Esta Função não existe",
+      ]);
+    }
+    //verificar se o banco existe
+    const verificarBanco = await prisma.banco.findFirst({
+      where: {
+        id: parseFuncionario.data.Id_banco,
+      },
+    });
+    if (!verificarBanco) {
+      throw new CustomError("Este Banco não existe!", 400, [
+        "Esta Banco não existe!",
+      ]);
+    }
+    //verificar se a categoria existe
+    const verificarCategoria = await prisma.categoriaRH.findFirst({
+      where: {
+        id: parseFuncionario.data.id_categoria,
+      },
+    });
+    if (!verificarCategoria) {
+      throw new CustomError("Esta Categria não existe!", 400, [
+        "Esta Categoria não existe!",
+      ]);
+    }
     const dados = await CreateFuncionario(parseFuncionario.data);
     return res.status(201).json({ massage: "Created Funcionario!", dados });
   } catch (err) {
